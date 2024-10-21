1c8687ccaad5d0dafbc35a246409ce3d25bd1464
Priscila Rodriguez Fresco <priscila.rodriguezfresco@gmail.com>
2024-10-21 19:49:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split('').reverse().join('');
 }
+
+console.log(isPalindrome("radar"));
+console.log(isPalindrome("rotator"));
+console.log(isPalindrome("refer"));
+console.log(isPalindrome("kayak"));
 
 /* 
   Add your pseudocode here
+
+  1. Create a function called isPalindrome
+  2. Function receives a string as an argument
+  4. Check if the string is the same backwards as it is forwards
+  5. Function returns true if the string is indeed a palindrome
+  6. Funciton returns false if it is not a palindrome
+  7. Test code using console.log()
 */
 
 /*
   Add written explanation of your solution here
+
+  Created a function named isPalindrome. Used the parameter 'name'
+  as a placeholder for all possible palindromes that will pass by. 
+  The function returns the value and the strict equality operator
+  checks if both values are the same by splitting, reversing, and
+  joining the values back together. The strict eqiality operator 
+  does not convert anything. The value and the type must be the 
+  same for the comparison to be true. If not, the result is false.
+  The function is then tested to see if it works by using the
+  console.log() built-in funciton.
 */
 
 // You can run `node index.js` to view these console logs
